bcef9ea9e5b7c085a5479b121776fd6c07b227bb
federicotempo <federicotempo98@gmail.com>
2025-01-08 12:02:53 -0300

    Create Gameboard class and tests
    -Creates gameboard class.
    -Creates gameboard methods.
    -Creates gameboard's unit tests .

--- src/logic.js
+++ src/logic.js
@@ -14,4 +14,57 @@ class Ship {
   }
 }
 
-module.exports = Ship;
+class Gameboard {
+  constructor(size = 10) {
+    this.size = size;
+    this.board = Array(size)
+      .fill(null)
+      .map(() => Array(size).fill(null));
+    this.missedAttacks = [];
+    this.ships = [];
+  }
+
+  placeShip(ship, x, y, direction) {
+    if (this.isValidPlacement(ship, x, y, direction)) {
+      for (let i = 0; i < ship.length; i++) {
+        if (direction === "horizontal") {
+          this.board[y][x + i] = ship;
+        } else if (direction === "vertical") {
+          this.board[x][y + i] = ship;
+        }
+      }
+      this.ships.push(ship);
+      return true;
+    }
+    return false;
+  }
+
+  isValidPlacement(ship, x, y, direction) {
+    if (direction === "horizontal") {
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[y][x + i] !== null) return false;
+      }
+    } else if (direction === "vertical") {
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[x][y + i] !== null) return false;
+      }
+    }
+    return true;
+  }
+
+  receiveAttack(x, y) {
+    if (this.board[y][x] !== null) {
+      this.board[y][x].hit();
+      return true;
+    } else {
+      this.missedAttacks.push((x, y));
+      return false;
+    }
+  }
+
+  allSunk() {
+    return this.ships.every((ship) => ship.isSunk());
+  }
+}
+
+module.exports = { Ship, Gameboard };
--- src/logic.test.js
+++ src/logic.test.js
@@ -1,7 +1,7 @@
-const Ship = require("./logic");
+const { Ship, Gameboard } = require("./logic");
 
 describe("Unit tests for the Ship class", () => {
-  it("hit should increase the number of 'hits' in the ship ", () => {
+  it("hit should increase the number of 'hits' in the ship", () => {
     const ship = new Ship("Carrier", 1);
     ship.hit();
     expect(ship.hitStatus).toBe(1);
@@ -9,7 +9,7 @@ describe("Unit tests for the Ship class", () => {
 
   it("isSunk should return true if the ship is sunk", () => {
     const ship = new Ship("Carrier", 1);
-    ship.hit()
+    ship.hit();
     expect(ship.isSunk()).toBe(true);
   });
 
@@ -18,5 +18,63 @@ describe("Unit tests for the Ship class", () => {
     ship.hit();
     expect(ship.isSunk()).toBe(false);
   });
+});
 
+describe("Unit tests for Gameboard class", () => {
+  it("placeShip should place a ship on the gameboard", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship("Carrier", 5);
+    const result = gameboard.placeShip(ship, 0, 0, "horizontal");
+    expect(result).toBe(true);
+    expect(gameboard.board[0][0]).toBe(ship);
+  });
+
+  it("receiveAttack should determine if the attack hit a ship", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship("Carrier", 5);
+    gameboard.placeShip(ship, 0, 0, "horizontal");
+    expect(gameboard.receiveAttack(4, 0)).toBe(true);
+  });
+
+  it("receiveAttack should determine if the attack misses a ship", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship("Carrier", 5);
+    gameboard.placeShip(ship, 0, 0, "horizontal");
+    expect(gameboard.receiveAttack(0, 1)).toBe(false);
+  });
+
+  it("receiveAttack should send the 'hit' function to the correct ship", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship("Carrier", 5);
+    gameboard.placeShip(ship, 0, 0, "horizontal");
+    gameboard.receiveAttack(4, 0);
+    expect(ship.hitStatus).toBe(1);
+  });
+
+  it("receiveAttack should record the coordinates of the missed shot", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship("Carrier", 5);
+    gameboard.placeShip(ship, 0, 0, "horizontal");
+    gameboard.receiveAttack(0, 1);
+    expect(gameboard.missedAttacks.length).toBe(1);
+  });
+
+  it("allSunk should report if all the ships have been sunk", () => {
+    const gameboard = new Gameboard();
+    const ship1 = new Ship("Carrier1", 1);
+    gameboard.placeShip(ship1, 0, 0, "horizontal");
+    const ship2 = new Ship("Carrier2", 1);
+    gameboard.placeShip(ship2, 1, 1, "horizontal");
+    gameboard.receiveAttack(0, 0);
+    gameboard.receiveAttack(1, 1);
+    expect(gameboard.allSunk()).toBe(true);
+  });
+
+  it("allSunk should return false if any of the ships have not been sunk", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship("Carrier", 1);
+    gameboard.placeShip(ship, 0, 0, "horizontal");
+    gameboard.receiveAttack(1, 1);
+    expect(gameboard.allSunk()).toBe(false);
+  });
 });
